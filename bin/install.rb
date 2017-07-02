a8a4dc6fe3c5e9b88007134165fdf421772106c5
Nobody Nowhere <nobody@example.com>
2017-07-02 02:28:10 -0700

    Add an install script
    The install script is ruby for now
    It dumps all the regalia content into a rags dir and
    tries to make sure the story name is correct

---
+++ bin/install.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+unless ARGV.length === 1
+  puts <<-EOT
+usage: #{$0} directory_to_exported_rags_project
+  EOT
+end
+
+rags_project_dir = ARGV[0]
+src_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'src'))
+
+puts "Searching '#{rags_project_dir}' for a .html file..."
+
+html_file = Dir["#{rags_project_dir}/*.html"].reject { |path| path.match /regalia/ }[0]
+unless html_file
+  puts "Found nothing, giving up..."
+  exit 1
+end
+puts "Found: '#{html_file}'"
+
+puts "Scraping title out of filename..."
+content = File.read(html_file)
+
+match = content.match(/<header.*?<b>(.*?)<\/b>.*?<\/header>/m)
+unless match
+  puts "Found nothing, giving up..."
+  exit 1
+end
+
+game_title = match[1]
+puts "Found: '#{game_title}'"
+puts "Copying Regalia content\n  From: '#{src_dir}'\n  To: #{rags_project_dir}"
+
+FileUtils.rm_rf(File.join(rags_project_dir, 'regalia.html'))
+html_content = File.read(File.join(src_dir, 'regalia.html'))
+File.write(File.join(rags_project_dir, 'regalia.html'), html_content.sub('GAME_TITLE_HERE', game_title))
+
+%w(js_vendor regalia_css regalia_js).each do |folder|
+  FileUtils.rm_rf(File.join(rags_project_dir, folder))
+  FileUtils.copy_entry(File.join(src_dir, folder), File.join(rags_project_dir, folder))
+end
+
+puts "Done!"

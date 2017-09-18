0ec14a0a647f79c6ff4781b230a032ec8bd85aea
Nobody Nowhere <nobody@example.com>
2017-09-17 22:04:10 -0700

    Always peel off empty command stacks during 'nextCommand' or 'shiftCommand'
    Fixes an issue in Wife Trainer since probably cb5255284006ec0a4f2ab57a0ce746fa30a9f538

--- src/regalia/js/Utilities.js
+++ src/regalia/js/Utilities.js
@@ -1,38 +1,47 @@
 var CommandLists = {
     stack: [
-        []
+        {obj: null, commands: []}
     ],
 
-    addNestedCommandList: function () {
-        this.stack.unshift([]);
+    addNestedCommandList: function (obj) {
+        this.stack.unshift({obj: obj, commands: []});
     },
 
     nextCommand: function () {
-        return this.stack[0][0];
+        this.unshiftEmptyStacks();
+        return this.stack[0].commands[0];
     },
 
     shiftCommand: function () {
-        var command = this.stack[0].shift();
-        if (this.stack.length > 1 && this.stack[0].length === 0) {
-            this.stack.shift();
-        }
+        var command = this.stack[0].commands.shift();
+        this.unshiftEmptyStacks();
         return command;
     },
 
     addToFront: function (command) {
-        this.stack[0].unshift(command);
+        this.stack[0].commands.unshift(command);
     },
 
     addToEnd: function (command) {
-        this.stack[0].push(command);
+        this.stack[0].commands.push(command);
+    },
+
+    objectBeingActedUpon: function () {
+        return this.stack[0].obj;
     },
 
     commandCount: function () {
         var result = 0;
         for (var i = 0; i < this.stack.length; i++) {
-            result += this.stack[i].length;
+            result += this.stack[i].commands.length;
         }
         return result;
+    },
+
+    unshiftEmptyStacks: function () {
+        while (this.stack.length > 1 && this.stack[0].commands.length === 0) {
+            this.stack.shift();
+        }
     }
 };
 

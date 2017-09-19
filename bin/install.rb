63b03dcced374cf6d0c744e67dda8cd56b9270db
Nobody Nowhere <nobody@example.com>
2017-09-18 23:28:35 -0700

    Make the runner file regalia.html all the time so it can be easily updated by patches

--- bin/install.rb
+++ bin/install.rb
@@ -15,31 +15,38 @@ node_modules_dir = File.join(root_dir, 'node_modules')
 
 puts "Searching '#{rags_project_dir}' for a .html file..."
 
-html_file = Dir["#{rags_project_dir}/*.html"].reject { |path| path.match /regalia/ }[0]
-html_file ||= Dir["#{rags_project_dir}/*.html.bak"].reject { |path| path.match /regalia/ }[0]
+html_file = Dir["#{rags_project_dir}/*{.html.bak,.html}"].reject { |path| path.match /regalia/ }[0]
 unless html_file
   puts "Found nothing, giving up..."
   exit 1
 end
 puts "Found: '#{html_file}'"
 
+if html_file.end_with?('.html')
+  FileUtils.mv(html_file, "#{html_file}.bak")
+end
+
 game_title = File.basename(html_file, '.*')
 puts "Copying Regalia content\n  From: '#{src_dir}'\n  To: #{rags_project_dir}"
 
 html_content = File.read(File.join(src_dir, 'regalia.html'))
-FileUtils.rm_rf(Dir[File.join(rags_project_dir, '* - regalia.html')])
-File.write(File.join(rags_project_dir, "#{game_title.strip} - regalia.html"), html_content.gsub('GAME_TITLE_HERE', game_title))
+FileUtils.rm_rf(Dir[File.join(rags_project_dir, '*regalia.html')])
+File.write(File.join(rags_project_dir, "regalia.html"), html_content.gsub('GAME_TITLE_HERE', game_title))
 
 FileUtils.rm_rf(File.join(rags_project_dir, 'regalia'))
 FileUtils.copy_entry(File.join(src_dir, 'regalia'), File.join(rags_project_dir, 'regalia'))
-FileUtils.copy(File.join(node_modules_dir, 'deep-diff', 'releases', 'deep-diff-0.3.8.min.js'), File.join(rags_project_dir, 'regalia', 'vendor'))
 FileUtils.copy(File.join(src_dir, 'Rags_32512.ico'), File.join(rags_project_dir, 'regalia'))
+
+vendor_files = [File.join(node_modules_dir, 'deep-diff', 'releases', 'deep-diff-0.3.8.min.js')]
+vendor_files.each do |file_path|
+  FileUtils.copy(file_path, File.join(rags_project_dir, 'regalia', 'vendor'))
+end
 
 puts "Copying Game.js content into Regalia folder..."
 FileUtils.mkdir_p(File.join(rags_project_dir, 'regalia', 'game'))
 
 game_js_content = File.read(File.join(rags_project_dir, 'js', 'Game.js'))
-if (game_js_content.match(%r(\\")))
+if game_js_content.match(%r(\\"))
   # replace double-backslashes followed by a double quote in Game.js text with triple-backslashes
   puts 'Found some suspicious escape sequences (\\\\") in Game.js'
   puts '  attempting to replace with (\\\\\\")'

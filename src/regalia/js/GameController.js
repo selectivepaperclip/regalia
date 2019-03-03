4f95479fdbb3b543aeceee63382f2c5b2c9c1278
Nobody Nowhere <nobody@example.com>
2019-03-03 13:39:46 -0800

    Fix/hack to not crash when selecting a menu option spawned by a live timer
    Previously if a live timer triggered a selection menu, the command
    processing code would chew up the whole command list which would cause
    input menu to not know what action had spawned the menu, and crash.
    
    Now, if user input is requested while a live timer is running, the command
    processing code stops processing commands until the input has been
    entered, even if those commands also come from a live timer.
    
    There are probably places that this behaves worse than the desktop RAGS
    code, but hopefully not too many games exercise live timer functionality.
    
    this particular fix targets Tessa's chess game in the sinner

--- src/regalia/js/GameController.js
+++ src/regalia/js/GameController.js
@@ -14,6 +14,16 @@ var GameController = {
 
     startAwaitingInput: function () {
         this.gameAwaitingInput = true;
+
+        // Somewhat of a hack: normally if commands executed as the result of a live timer
+        // the are allowed to succeed even if the game is paused. In the desktop RAGS client,
+        // live timer events seem to keep happening even if input is requested from the user.
+
+        // That's hard to implement well, so this just makes it so if the player is asked for
+        // input it doesn't try to clear out the rest of the event queue until execution
+        // resumes normally
+        Globals.runningLiveTimerCommands = false;
+
         GameUI.disableSaveAndLoad();
         GameUI.hideGameElements();
     },
